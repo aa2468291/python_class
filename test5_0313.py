492eb3e466753d82905d87b5743a2b67bb37da45
aa2468291 <aa2468291@gmail.com>
2020-03-13 12:01:11 +0800

    An

---
+++ test5_0313.py
@@ -0,0 +1,14 @@
+
+# sum = 0
+# for i in range(51, 302):
+#     if i % 3 == 0:
+#         sum = sum + i
+#
+# print(sum)
+
+total = 0
+
+for i in range(51, 302, 3):
+    total = total + i
+
+print(total)
---
+++ test6_0313.py
@@ -0,0 +1,18 @@
+x = 1
+y = 2
+n = int(input('請輸入A[n]'))
+
+
+for i in range(n-1):
+    z = 3*y + 5*x + 1
+    x = y
+    y = z
+
+print(z)
+
+
+
+
+
+
+
