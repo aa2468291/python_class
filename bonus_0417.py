748173f55379a9aa93e2dc5e78a9a2a83e488716
aa2468291 <aa2468291@gmail.com>
2020-04-17 22:07:50 +0800

    0417 bonus 1 finished

--- bonus_0417.py
+++ bonus_0417.py
@@ -2,4 +2,11 @@ students = {'Chang': {'Math': 90, 'Chinese': 85, 'English': 60},
             'Tang': {'Math': 85, 'Chinese': 70, 'English': 92},
             'Chen': {'Math': 90, 'Chinese': 93, 'English': 95}}
 
+new = {}
 
+for student in students:
+    for grade in students[student]:
+        new.setdefault(grade, {})
+        new[grade].setdefault(student, students[student][grade])
+
+print(new)
--- test13_0417.py
+++ test13_0417.py
@@ -2,6 +2,10 @@ students = {'Chang': {'Math': 90, 'Chinese': 85, 'English': 60},
             'Tang': {'Math': 85, 'Chinese': 70, 'English': 92},
             'Chen': {'Math': 90, 'Chinese': 93, 'English': 95}}
 
+pre = []
+rank = []
+index = 0
+
 for student in students:
     # students[student].setdefault('total', 0)
     total = 0
@@ -10,7 +14,19 @@ for student in students:
         total = total + students[student][grade]
 
     students[student].setdefault('total', total)
+    pre.append(total)
 
     # print(students[student][grade])
+
+for a in pre:
+    base = len(pre)
+    for b in pre:
+        if a > b:
+            base = base - 1
+    rank.append(base)
+
+for student in students:
+    students[student].setdefault('rank', rank[index])
+    index = index + 1
 
 print(students)
