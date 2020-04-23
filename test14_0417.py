41699198c6e88acde3c53d4c407b7d2baefe9bfd
aa2468291 <aa2468291@gmail.com>
2020-04-23 23:13:40 +0800

    finished 0417

--- test14_0417.py
+++ test14_0417.py
@@ -1,18 +1,20 @@
-import random
+while True:
+    UID = input('請輸入身份證字號')
+    if UID[0].isupper() and len(UID) == 10 and (UID[1] == '1' or UID[1] == '2'):
+        break
+    else:
+        print("格式錯誤，再試一次")
 
 rule = [10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33]
 weight = [1, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-alphabet = chr(random.randint(0, 25)+65)
-number = random.randint(10000000, 29999999)
-num = rule[ord(alphabet) - 65]
-
+num = rule[ord(UID[0]) - 65]
 pre = []
 total = 0
 
 pre.append(int(num / 10))
 pre.append(num % 10)
 
-for i in str(number)[0:8]:
+for i in UID[1:9]:
     pre.append(int(i))
 # print(pre)
 
@@ -21,11 +23,7 @@ for j in range(len(pre)):
 
 # print(UID[9])
 # print(total % 10)
-check = int(10 - total % 10)
-if check == 10:
-    check = 0
-
-print(pre)
-print(total)
-
-print(str(alphabet)+str(number)+str(check))
+if int(10 - total % 10) == int(UID[9]):
+    print("正確")
+else:
+    print("錯誤")
